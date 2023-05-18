9b8246fe187f9e3b26dc3c09dc5372eac69dbcd7
Janu Kumar Singh <119485859+jks6404@users.noreply.github.com>
2023-05-18 18:27:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+
+let selectedTiles = [];
+ 
+function selectTile(tile) {
+  if (selectedTiles.length < 2) {
+    selectedTiles.push(tile);
+    tile.classList.add("selected");
+    document.getElementById("reset").style.display = "block";
+  }
+  if (selectedTiles.length === 2) {
+    document.getElementById("verify").style.display = "block";
+  }
+}
+ 
+function reset() {
+  selectedTiles.forEach(tile => {
+    tile.classList.remove("selected");
+  });
+  selectedTiles = [];
+  document.getElementById("reset").style.display = "none";
+  document.getElementById("verify").style.display = "none";
+  document.getElementById("para").style.display = "none";
+}
+ 
+function verify() {
+  if (selectedTiles[0].className === selectedTiles[1].className) {
+    document.getElementById("para").innerHTML = "You are a human. Congratulations!";
+  } else {
+    document.getElementById("para").innerHTML = "We can't verify you as a human. You selected the non-identical tiles.";
+  }
+  document.getElementById("reset").style.display = "none";
+  document.getElementById("verify").style.display = "none";
+  document.getElementById("para").style.display = "block";
+}
